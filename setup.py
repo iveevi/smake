6f1aec72386dfd7ba37cb471a0a3f3233e1791ca
Venkataram <vesion4690@gmail.com>
2022-11-11 11:59:01 -0800

    Final update for this version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name = 'smake',
-        version = '1.2.5',
+        version = '1.2.6',
         scripts = ['smake'],
         author = "Venkataram Edavamadathil Sivaram",
         author_email = "vesion4690@gmail.com",
